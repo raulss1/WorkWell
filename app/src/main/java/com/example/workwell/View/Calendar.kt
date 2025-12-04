525ebd419788d0ad6d668acbbb26a852b48627bc
Magickio <dad2dadad@gmail.com>
2025-12-04 18:54:16 +0000

    Creada la vista del calendario

---
+++ app/src/main/java/com/example/workwell/View/Calendar.kt
@@ -0,0 +1,15 @@
+package com.example.workwell.View
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Calendar(modifier: Modifier = Modifier)
+{
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text("Calendar View")
+    }
+}
