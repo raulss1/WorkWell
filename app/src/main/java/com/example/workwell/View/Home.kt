aed895816777cbd30c045747664dd35f938b12c6
Magickio <dad2dadad@gmail.com>
2025-12-14 19:23:05 +0000

    Footer arreglado en el home

--- app/src/main/java/com/example/workwell/View/Home.kt
+++ app/src/main/java/com/example/workwell/View/Home.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -258,6 +261,7 @@ fun HomeView(name: String, tasks: List<Task>) {
             Box(
                 modifier = Modifier
                     .align(Alignment.BottomCenter) // Pegado abajo
+                    .windowInsetsPadding(WindowInsets.systemBars)
                     .fillMaxWidth()
             ) {
                 // TRUCO DE SOMBRA HACIA ARRIBA
